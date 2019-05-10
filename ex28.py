64236765ccf3c7bd649a201173ec87647c97bebc
Raymand Martinez <student.133192@worcesterschools.net>
2019-05-10 13:04:18 +0000

    24-35

---
+++ ex28.py
@@ -0,0 +1,11 @@
+ True and True 
+ False and True 
+ 1 == 1 and 2 == 1
+ "test" == "test"
+ 1 == 1 or 2 != 1
+ true and 1 == 1 
+false and 0 != 0
+true or  1 == 1
+"test" == "testing"
+1 != 0 and 2 == 1 
+"test" 
---
+++ ex31.py
@@ -0,0 +1,38 @@
+print("""you enter a dark room with two doors
+Do you go through door #1 or door #2?""")
+
+door = input("> ")
+
+if door == "1":
+    print("There's a giant bear here eaeting a cheese cake.")
+    print("what do you do?")
+    print("1. Take the cake.")
+    print("2. Scream a the bear")
+    
+    bear = input("> ")
+    
+    if bear == "1":
+        print("The bear eats your face off. Good job!")
+    elif bear == "2":
+        print("The bear eats your legs off. Good Job!")
+    else:
+         print(f"Well, doing {bear} is probably better.")
+         print("Bears runs away.")
+            
+            
+elif door == "2":
+    print("You stare into the endless abyss at Cthulu's retina.")
+    print("1. Blueberries.")
+    print("2. Yellow jackets clothespins.")
+    print("3. Understanding revolers yelling melodies.")
+    
+    insanity = input("> ")
+    
+    if insanity == "1" or insanity == "2" or insanity == "3":
+        print("Your body survives powered by a mind of jello.")
+        print("Good Job!")
+        print("With your big brian you end up while walking away slipping on a knife then dying")
+        
+
+    
+   
